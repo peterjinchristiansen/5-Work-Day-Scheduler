20ad74b773a4fa4ef804c7a04289b3ea28768752
peterjinchristiansen <82626937+peterjinchristiansen@users.noreply.github.com>
2021-09-02 20:39:28 -0600

    moment changes
    properly set the current date at the top, created the html for the boxes, color coded each box based on past/present/future

--- script.js
+++ script.js
@@ -1,9 +1,36 @@
-var date = document.getElementById("currentDay");
- 
+var descriptionClass = document.getElementsByClassName("description");
+
 window.onload = getCurrentDate();
+
+$('.description').click(function(){
+//when you type, the input is put into the html of description
+});
+
+$('.saveBtn').click(function(){
+//save the content of the nearby description to local storage
+})
  
 function getCurrentDate() {
-    var currentDate = moment().format("MMMM Do YYYY");
-    date.innerHTML = currentDate;
+    var currentDate = moment().format("dddd, MMMM Do");
+    $('#currentDay').html(currentDate);
+    var currentTime = moment().format("kk");
+    colorCode(currentTime);
 }
 
+function colorCode(currentTime) {
+    var currentTimeNum = parseInt(currentTime);
+    for(i = 0; i < descriptionClass.length; i++) {
+        if(currentTimeNum > i + 9) {
+            $(descriptionClass[i]).addClass('past');
+        } else if(currentTimeNum = i + 9) {
+            $(descriptionClass[i]).addClass('present');
+        } else {
+            $(descriptionClass[i]).addClass('future');
+        }
+    }
+    grabPreviousEvents();
+}
+
+function grabPreviousEvents() {
+//grab items from local storage and put them into the respective boxes
+}
